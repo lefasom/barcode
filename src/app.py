ef1bcbc59a4453aaecc1e8d1290b2cf4fa1afc3c
lefasom1993 <lefasom1993@gmail.com>
2023-12-19 14:09:03 -0300

    modifico codigo

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-import os
 import cv2
-from pyzbar import pyzbar
+import zbarlight
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 barcode_value = "None" 
+
 def barcode():
     global barcode_value
     cap = cv2.VideoCapture(0)
@@ -12,24 +12,28 @@ def barcode():
         ret, frame = cap.read()
         if not ret:
             break
-        barcodes = pyzbar.decode(frame)
         
-        for barcode in barcodes:
-            (x,y,w,h) = barcode.rect
-            cv2.rectangle(frame, (x,y), (x+w, y+h),(0,255,0),2)
-            barcode_value = barcode.data.decode("utf-8")
-            cv2.putText(frame, barcode_value,(x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.0,(0,255,0),2)
+        # Convertir la imagen de la cámara a escala de grises para zbarlight
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # Escanear códigos de barras en la imagen en escala de grises
+        codes = zbarlight.scan_codes('qrcode', gray)
+        
+        if codes is not None:
+            barcode_value = codes[0].decode("utf-8")
             
-        cv2.imshow("barcode Scanner", frame)
-        if cv2.waitkey(1) & 0xFF == ord('q'):
+        cv2.imshow("Barcode Scanner", frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+            
     cap.release()
     cv2.destroyAllWindows()
 
 
-@app.route("/") #decorador
+@app.route("/")
 def Home():
-    return render_template('index.html', barcode_value=barcode_value)  # Pasar el valor del código de barras a la plantilla HTML
+    return render_template('index.html', barcode_value=barcode_value)
 
 
 @app.route('/video_feed')
@@ -38,4 +42,3 @@ def video_feed():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
