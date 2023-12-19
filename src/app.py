e8a983300dfac95a2db9732d0cde7249339b3bd9
lefasom1993 <lefasom1993@gmail.com>
2023-12-19 15:52:13 -0300

    probando

--- src/app.py
+++ src/app.py
@@ -1,30 +1,30 @@
 import os
 import cv2
-from pyzbar import pyzbar
+from playsound import playsound
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 barcode_value = "None" 
 def barcode():
     global barcode_value
+    bd = cv2.barcode.BarcodeDetector()
     cap = cv2.VideoCapture(0)
-    while cap.isOpened():
+    
+    deteccion = {}
+    
+    while True:
         ret, frame = cap.read()
-        if not ret:
-            break
-        barcodes = pyzbar.decode(frame)
-        
-        for barcode in barcodes:
-            (x,y,w,h) = barcode.rect
-            cv2.rectangle(frame, (x,y), (x+w, y+h),(0,255,0),2)
-            barcode_value = barcode.data.decode("utf-8")
-            cv2.putText(frame, barcode_value,(x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.0,(0,255,0),2)
-            
-        cv2.imshow("barcode Scanner", frame)
-        if cv2.waitkey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+        if ret:
+            (
+                ret_bc,
+                decode,
+                _,
+                puntos
+            ) = bd.detectAndDecode(frame)
+            if ret_bc:
+                barcode_value = decode
+       
+                
 
 
 @app.route("/") #decorador
